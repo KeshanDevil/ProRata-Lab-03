58ec6bc1b376c4c6d531dc5ddbaa4cddfc75b555
KeshanDevil <87665135+KeshanDevil@users.noreply.github.com>
2026-09-16 01:52:24 +0530

    Add files via upload

---
+++ Q1A/IT21047756Lab3Q1A.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class IT21047756Lab3Q1A {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the price of 1kg of rice: ");
+        double price = input.nextDouble();
+
+        System.out.print("Enter the number of kilograms: ");
+        double kilograms = input.nextDouble();
+
+        double amount = price * kilograms;
+
+        System.out.println("Amount you have to pay = " + amount);
+
+        input.close();
+    }
+}
